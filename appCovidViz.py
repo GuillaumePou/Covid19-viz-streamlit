a65606a6a89e36ade47666be94d79e82bfac4e4d
GuillaumePou <GuillaumePou@users.noreply.github.com>
2020-04-09 23:09:06 +0200

    change to country name and add auto dayly update

--- appCovidViz.py
+++ appCovidViz.py
@@ -11,17 +11,25 @@ import pandas as pd
 import altair as alt
 import numpy as np
 import plotly.express as px
+from datetime import date
 
+today = date.today().day
 
 #%% Intialize paths
 
-path2csv = "Data/covid19_casedistribution.csv"
+@st.cache
+def retrieveDF(today):
+    from io import StringIO
+    import requests
+    urlCovid19 = "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
+    df = pd.read_csv(StringIO(requests.get(urlCovid19).text), sep=",")
+    return df
 
 #%% load df and process
 
 @st.cache
-def processData(path2csv):
-    df = pd.read_csv(path2csv, sep=",", encoding='latin-1')
+def processData(df):
+#    df = pd.read_csv(path2csv, sep=",", encoding='latin-1')
     df.dateRep = pd.to_datetime(df.dateRep, format="%d/%m/%Y")
     
     #add cumulative deaths and confirmed cases
@@ -33,7 +41,7 @@ def processData(path2csv):
     df['daysAfter10days'] = df[df.deathsCum > 10].groupby('geoId').deaths.rank(method="first", ascending=True)
     return(df)
 
-df = processData(path2csv)
+df = processData(retrieveDF(today))
 
 #%% functions
 
@@ -81,26 +89,26 @@ if typeSelectbox == "charts":
     
         if st.checkbox('log scale'):
             
-            chartDeaths = chart(df, 'dateRep', 'deathsCum', 'countryterritoryCode', 'log')
-            chartCases = chart(df, 'dateRep', 'casesCum', 'countryterritoryCode', 'log')
+            chartDeaths = chart(df, 'dateRep', 'deathsCum', 'countriesAndTerritories', 'log')
+            chartCases = chart(df, 'dateRep', 'casesCum', 'countriesAndTerritories', 'log')
             
             st.altair_chart(chartDeaths, use_container_width=True)
             st.altair_chart(chartCases, use_container_width=True)
         
         else:
-            chartDeaths = chart(df, 'dateRep', 'deathsCum', 'countryterritoryCode')
-            chartCases = chart(df, 'dateRep', 'casesCum', 'countryterritoryCode')
+            chartDeaths = chart(df, 'dateRep', 'deathsCum', 'countriesAndTerritories')
+            chartCases = chart(df, 'dateRep', 'casesCum', 'countriesAndTerritories')
             
             st.altair_chart(chartDeaths, use_container_width=True)
             st.altair_chart(chartCases, use_container_width=True)    
         
         if st.checkbox('Fatality rate'):
-            chartFatality = chart(df, 'dateRep', 'fatalityRate', 'countryterritoryCode')
+            chartFatality = chart(df, 'dateRep', 'fatalityRate', 'countriesAndTerritories')
             st.altair_chart(chartFatality, use_container_width=True)
 
     else:
-        chartDeaths = chart(df, 'dateRep', 'deaths', 'countryterritoryCode')
-        chartCases = chart(df, 'dateRep', 'cases', 'countryterritoryCode')
+        chartDeaths = chart(df, 'dateRep', 'deaths', 'countriesAndTerritories')
+        chartCases = chart(df, 'dateRep', 'cases', 'countriesAndTerritories')
         
         st.altair_chart(chartDeaths, use_container_width=True)
         st.altair_chart(chartCases, use_container_width=True)
@@ -113,7 +121,15 @@ elif typeSelectbox == "map":
 
     # nbDaySinceOutbreak = df.dateRep.max() - df.dateRep.min()
     # date = st.slider('Date', 0, 1, nbDaySinceOutbreak)
-    dfm = df[df['dateRep']==lastDay]
+    
+    
+    if st.checkbox('not logarithmic scale'):
+        dfm = df[df['dateRep']==lastDay]
+        
+    else:
+        dfm = df[df['dateRep']==lastDay]
+        dfm["deathsCum"] = np.log10(dfm.deathsCum)
+        
     fig = px.choropleth(dfm, locations="countryterritoryCode",
                         color="deathsCum", # lifeExp is a column of gapminder
                         hover_name="countriesAndTerritories", # column to add to hover information
